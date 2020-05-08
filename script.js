9696ab8d4bba6d9ad9c82cbf070a5e5640dc384c
TJReynolds88 <tj.reynolds1988@gmail.com>
2020-05-08 16:39:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -40,6 +40,58 @@ function timer() {
     seconds --;
     if(seconds < 0) {
         clearInterval(countdown);
-        seconds = (isBreak ? bre)
+        seconds = (isBreak ? breakTime : workTime)* 60;
+        isBreak = !isBreak;
+        countdown = setInterval(timer, 1000);
     }
 }
+
+
+/* Update the Break & Work Times */
+let increment = 5;
+
+let incrementFunctions = {
+    "#work-plus": function () {
+    workTime = Math.min(workTime + increment, 60)},
+    "#work-minus": function () {
+    workTime = Math.max( workTime - increment, 5)},
+    "break-plus": function () {
+    breakTime = Math.min(breakTime + increment, 60)},
+    "#break-minus": function () {
+    breakTime = Math.max( breakTime - increment, 5)}};
+
+for (var key in incrementFunctions) {
+    if (incrementFunctions.hasOwnProperty(key)) {
+        document.querySelector(key).onclick = incrementFunctions[key];
+    }
+}
+
+/* Update DOM */
+
+function countdownDisplay () {
+    let minutes = Math.floor(seconds / 60);
+    let remainderSeconds = seconds % 60;
+    timerDisplay.textContent = `${minutes}:${remainderSeconds < 10 ? '0': ''}${remainderSeconds}`;
+}
+
+function buttonDisplay() {
+    if (isPaused && countdown === 0) {
+        startBtn.textContent = "START";
+    }else if (isPaused && countdown !== 0) {
+        startBtn.textContent = "Continue";
+    } else {
+        startBtn.textContent = "Pause";
+    }
+}
+
+function updateHTML() {
+    countdownDisplay();
+    buttonDisplay();
+    isBreak ? status.textContent = "Keep Working" : status.textContent = "Take a Break!";
+    workMin.textContent = workTime;
+    breakMin.textContent = breakTime;
+}
+
+window.setInterval(updateHTML,100);
+
+document.onclick = updateHTML;
